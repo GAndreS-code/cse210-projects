dc757494af2ec12b80373310cf73f7ff0a239c51
gandres-code <gandres.code@gmail.com>
2024-01-19 18:19:19 +0800

    learning02 file update

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Amazon";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        job1.Display();
+        job2.Display();
     }
 }
