13ea9640a9b424ef333041e33ef05159396da697
gandres-code <gandres.code@gmail.com>
2024-01-23 16:47:19 +0800

    Prep3 assignment complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber) {
+            Console.Write( "Guess the number. ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess) {
+                Console.WriteLine("Nope! Try a higher number.");
+            } 
+            else if (magicNumber < guess) {
+                Console.WriteLine("Wrong! Give me a lower number.");
+            }
+            else {
+                Console.WriteLine("You got it!");
+            }
+
+        }
     }
 }
